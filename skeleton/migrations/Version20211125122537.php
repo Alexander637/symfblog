e745c35f96cf4d83c82b15a61bfc4b4161b7185f
Alexander637 <sadorsadoff@gmail.com>
2021-11-25 14:41:29 +0200

    Database sessions config

---
+++ skeleton/migrations/Version20211125122537.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211125122537 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('
+                CREATE TABLE session (
+                    session_id VARCHAR(128) NOT NULL PRIMARY KEY,
+                    session_data BYTEA NOT NULL,
+                    session_lifetime INTEGER NOT NULL,
+                    session_time INTEGER NOT NULL
+                )'
+        );
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+    }
+}
